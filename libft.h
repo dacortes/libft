42e65250305ec35cb345b1ab01a09ce82ed286e3
dacortes <dacortes@student.42barcelona.com>
2023-05-17 11:13:11 +0200

    update ft_double_ptrlen

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 14:00:29 by dacortes          #+#    #+#             */
-/*   Updated: 2023/05/11 15:29:44 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/05/17 11:11:53 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	**ft_split(char const *s, char c);
 char	*ft_addend_char(const char *str, const char c);
 char	*ft_addstart_char(const char *str, const char c);
+int		ft_double_ptr_len(void **ptr);
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
