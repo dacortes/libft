997ca0a1c4a2ac2bd47fcf78e1c94db399423d73
dacortes <dacortes@student.42barcelona.com>
2023-07-13 17:00:35 +0200

    add close del

---
+++ ft_close_del.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_close_del.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 16:59:49 by dacortes          #+#    #+#             */
+/*   Updated: 2023/07/13 17:00:13 by dacortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libc.h"
+
+int	close_del(char *str, char delimiter)
+{
+	int	del;
+	int	i;
+
+	del = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == delimiter)
+			del++;
+		else if (str[i] == delimiter && del % 2 == 0)
+		{
+			while (str[i])
+			{
+				i++;
+				if (str[i] == delimiter)
+				{
+					del++;
+					break ;
+				}
+			}
+		}
+		i++;
+	}
+	return (del % 2 == 0);
+}
