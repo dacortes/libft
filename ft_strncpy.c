6b3a3941070f5b203e9edcba05c4edc47ceef4e6
dacortes <dannyel.cortesr@gmail.com>
2023-09-12 16:33:02 +0200

    [ADD] ft_* in libft

---
+++ ft_strncpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 16:25:10 by dacortes          #+#    #+#             */
+/*   Updated: 2023/09/12 16:25:34 by dacortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strncpy(char *dst, const char *src, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && i < len)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (i < len && src[i] == '\0')
+	{
+		while (dst[i])
+			dst[i++] = '\0';
+	}
+    return (dst);
+}
